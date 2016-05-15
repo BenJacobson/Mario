5d0fff0505ec0d25fc85e6c78f95e90daf1f53e2
BenTJ <Ben.Jacobson@microfocus.com>
2016-05-15 11:52:40 -0600

    Include class files from jinput directly in JAR to allow project to be compiled as a standalone application

--- src/util/input/GameController.java
+++ src/util/input/GameController.java
@@ -9,7 +9,6 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.*;
 import java.lang.reflect.Field;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
